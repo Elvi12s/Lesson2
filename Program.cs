13ef664d49495a49dfd1dd883cf4ff2b1d0c189a
Elvis <elvisnepresly@yandex.ru>
2023-01-07 23:34:12 +0300

    File created

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("введите 3х значное число");
+int num= Convert.ToInt32(Console.ReadLine());
+int num2;
+num2=(num/10)%10;
+Console.WriteLine(num2);
